25c0e1b1e442d050052adf9e51c3512f1c3e0541
Hupiter <sirhcardet@gmail.com>
2018-03-12 14:29:14 -0400

    SSL verification support
    These allow to Turn off SSL Verification. Not recommended for deployments.

--- client.py
+++ client.py
@@ -1,31 +1,38 @@
-import requests
+from six.moves import urllib
 import json
+import ssl
+
 
 class GraphQLClient:
-    def __init__(self, endpoint, token=None):
+    def __init__(self, endpoint):
         self.endpoint = endpoint
-        self.token = token
+        self.token = None
 
-    def execute(self, query, variables=None):
-        return self._send(query, variables)
+    def execute(self, query, variables=None, verify_ssl=True):
+        return self._send(query, variables, verify_ssl)
 
     def inject_token(self, token):
         self.token = token
 
-    def _send(self, query, variables):
+    def _send(self, query, variables, verify_ssl):
         data = {'query': query,
                 'variables': variables}
         headers = {'Accept': 'application/json',
                    'Content-Type': 'application/json'}
 
         if self.token is not None:
-            headers['Authorization'] = 'Bearer %s' % self.token
+            headers['Authorization'] = 'Bearer {}'.format(self.token)
+
+        req = urllib.request.Request(
+            self.endpoint, json.dumps(data).encode('utf-8'), headers)
 
         try:
-            response = requests.post(self.endpoint, data=json.dumps(data), headers=headers)
-            # print(response.text)
-            return response.json()
-        except requests.RequestException as e:
-            print(e.read())
+            context = ssl.create_default_context()
+            if not verify_ssl:
+                context = ssl._create_unverified_context()
+            response = urllib.request.urlopen(req, context=context)
+            return response.read().decode('utf-8')
+        except urllib.error.HTTPError as e:
+            print((e.read()))
             print('')
             raise e
